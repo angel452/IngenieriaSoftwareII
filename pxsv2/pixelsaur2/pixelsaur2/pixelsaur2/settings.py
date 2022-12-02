5019306ba96b1c534180d9834dbd0ecd7760029b
Angel_Arch <angelloayzah2016@gmail.com>
2022-12-02 13:55:49 -0500

    icono admin

--- pxsv2/pixelsaur2/pixelsaur2/pixelsaur2/settings.py
+++ pxsv2/pixelsaur2/pixelsaur2/pixelsaur2/settings.py
@@ -191,7 +191,7 @@ JAZZMIN_SETTINGS = {
     "site_brand": "PixelSaur",
 
     # Logo to use for your site, must be present in static files, used for brand on top left
-    #"site_logo": "img/logo_pixelsaur.png",
+    "site_logo": "img/logo_pixelsaur.png",
 
     # CSS classes that are applied to the logo above
     #"site_logo_classes": "img-circle",
